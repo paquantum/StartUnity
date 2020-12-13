c2c3fe30622f9b37cac78b562cc09e94a1c91364
paquantum <enmw34@naver.com>
2020-12-13 17:47:15 +0900

    Update HUD and GunController.cs

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -257,4 +257,9 @@ public class GunController : MonoBehaviour
         audioSource.clip = _clip;
         audioSource.Play();
     }
+
+    public Gun GetGun()
+    {
+        return currentGun;
+    }
 }
